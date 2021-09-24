f880fbc21343202489893e91d412a6401ae73650
Udoka <udokakuugochukwu@gmail.com>
2021-09-24 17:16:23 +0100

    New updates

--- vrede.js
+++
@@ -1,8 +0,0 @@
-let navjs = document.getElementById('nav-head');
-        
-    navjs.addEventListener('scroll', () => {
-        navjs.style.background = '#ffffff';
-    })
-
-
-    // export("/vredejs")
